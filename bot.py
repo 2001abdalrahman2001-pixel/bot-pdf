29e89d5422d8adabc1f3b6a06f5f439477f4861b
2001abdalrahman2001-pixel <2001abdalrahman2001@gmail.com>
2026-03-24 10:02:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTyp
 from PIL import Image
 import os
 
-TOKEN = "8629476476:AAEe6mVzZcSolnv4VyIA1XMVlcK_fHB_hRk"
+TOKEN = "8629476476:AAFZhdoIs4_ak__SM5cFr02mlzKPsovZA48"
 
 async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     photos = update.message.photo
